23a67bd335cb6c010f17d7908cd910e11d45a2e1
shylux <shylux@6c3ce700-eceb-3ca8-9cf3-d9c6b2edddea>
2011-05-27 07:45:49 +0000

    Adde File to merge all file_get_contents. Usage over commandline.
    git-svn-id: https://php-browser.googlecode.com/svn/trunk@14 6c3ce700-eceb-3ca8-9cf3-d9c6b2edddea

---
+++ merge1file.php
@@ -0,0 +1,26 @@
+#!/usr/bin/php
+<?php
+
+$singleline = true;
+
+if (!isset($argv[1])) {
+	print "Please select the file.\n";
+	return;
+}
+
+$file = file_get_contents($argv[1]);
+
+$i = 2;
+while (isset($argv[$i])) {
+	//echo $argv[$i] . "\n";
+	$content = file_get_contents($argv[$i]);
+	$content = str_replace(array("\\", "'", "\t", "^M"), array("\\\\", "\'", "", ""), $content);
+	$content = trim($content, "\n");
+	if ($singleline) $content = str_replace("\n", "", $content);
+	$content = "'". $content ."';";
+	$search = "file_get_contents(\"".$argv[$i]."\");";
+	$file = str_replace($search, $content, $file);
+	$i++;
+}
+file_put_contents($argv[1], $file);
+?>
